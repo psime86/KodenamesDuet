363df19e395d312b4124f367927538f38d5bea41
David Koeller <david.koeller@gmail.com>
2020-03-18 21:31:53 -0500

    api routes/html routes updated

--- models/example.js
+++ models/example.js
@@ -1,6 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
-  var gameWords = sequelize.define("Words", {
-    word: DataTypes.STRING
-  });
-  return Example;
+  var gameWords = sequelize.define(
+    "Words",
+    {
+      word: DataTypes.STRING
+    },
+    {
+      timestamps: false
+    }
+  );
+  return gameWords;
 };
