0006423d80ca17110cbd1d16cf4ae78c4ad3c9d0
Patrick Sime <psime86@gmail.com>
2020-03-23 18:30:23 -0500

    pull recent

--- public/js/index.js
+++ public/js/index.js
@@ -1,46 +1,51 @@
 $(document).ready(function(){
-
-var cards = $(".back");
-
-var redTeam = document.createElement("img");
-redTeam.src = "'images/red-spy.jpg' style='width: 100%; height: 100%;'>";
-
-var bystander = document.createElement("img");
-bystander.src = "'images/bystander.jpg' style='width: 100%; height: 100%;'>";
-
-var blueTeam = document.createElement("img");
-blueTeam.src = "'images/blue-spy.jpg' style='width: 100%; height: 100%;'>";
-
-var assassin = document.createElement("img");
-assassin.src = "'images/black-spy.jpg' style='width: 100%; height: 100%;'>";
-
   
-  //create teams
-	for (var i = 0; i < 8; i++) {
-		cards.append(redTeam);
-		cards.append(blueTeam);
-	}
+  $(function() {
+    var parent = $("#Test");
+    var divs = parent.children();
+    while (divs.length) {
+      parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+    };
+});
 
-	// // one extra for one of the teams
-	// if (Math.floor(Math.random() * data.length) % 2 === 0) {
-	// 	cards.append(redTeam);
-	// 	$('#board').addClass('redStarts').removeClass('blueStarts');
+  // var cards = $(".back");
 
-	// } else {
-	// 	cards.append(blueTeam);
-	// 	$('#board').addClass('blueStarts').removeClass('redStarts');
-	// }
+  // var redTeam = "<img src='images/red-spy.jpg' style='width: 100%; height: 100%;'>";
 
-	// add neturals 
-	for (var i = 0; i < 7; i++) {
-		cards.append(bystander);
-	}
+  // var bystander = "<img src='images/bystander.jpg' style='width: 100%; height: 100%;'>";
 
-	// push the assasin
-	cards.append(assasin);
+  // var blueTeam = "<img src='images/blue-spy.jpg' style='width: 100%; height: 100%;'>";
 
-// var cards = "<div class='back'><img src='images/blue-spy.jpg' style='width: 100%; height: 100%;'></div>"
-// for (var i = 0; i < 8; i++){
-//   $(".card m-3").push(cards);
-// }
-})
+  // var assassin = "<img src= 'images/black-spy.jpeg' style='width: 100%; height: 100%;'>";
+
+  // //create teams
+  // for (var i = 0; i < 8; i++) {
+  //   $(cards).append(redTeam);
+  // };
+
+  // for (var i = 0; i < 8; i++) {
+  //   $(cards).append(blueTeam);
+  // };
+
+  // // one extra for one of the teams
+  // if (Math.floor(Math.random() * data.length) % 2 === 0) {
+  //   cards.append(redTeam);
+  //   $("#FirstTeam").html("Red Team Starts");
+  // } else {
+  //   cards.append(blueTeam);
+  //   $("#FirstTeam").html("Blue Team Starts");
+  // }
+
+  // add neturals
+  // for (var i = 0; i < 7; i++) {
+  //   cards.append(bystander);
+  // };
+
+  // // push the assasin
+  // cards.append(assassin);
+
+  // var cards = "<div class='back'><img src='images/blue-spy.jpg' style='width: 100%; height: 100%;'></div>"
+  // for (var i = 0; i < 8; i++){
+  //   $(".card m-3").push(cards);
+  // }
+});
---
+++ schema.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS words_db;
+CREATE DATABASE words_db;
+
+USE words_db;
+
+CREATE TABLE words(
+    words VARCHAR(30) NOT NULL
+);
