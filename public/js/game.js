ba2543f3a597619461b3692a5d212504c2855b64
David Koeller <david.koeller@gmail.com>
2020-03-23 19:01:18 -0500

    fresh pull

--- public/js/game.js
+++ public/js/game.js
@@ -2,3 +2,20 @@ $("#player-join").on('click', function() {
     console.log('clicked')
     $("#game-id").css('display', 'inline')
 });
+
+
+var wordArray = [];
+
+
+$.get("/api/words", function(data) {
+    for (var i = 0; i < data.length; i++) {
+        wordArray.push(data[i].word);
+    }
+});
+
+console.log(wordArray);
+
+for (i = 0; i < wordArray.length; i++) {
+    $(".card-title").text(wordArray[i]);
+}
+console.log(wordArray[i]);
